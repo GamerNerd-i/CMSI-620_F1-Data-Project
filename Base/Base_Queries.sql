77895621f0630a2eeee26b3dd83f4a16926579c4
Aidan Dionisio <aidanbcd@mac.com>
2024-12-06 14:40:18 -0800

    wrote some queries

--- Base/Base_Queries.sql
+++ Base/Base_Queries.sql
@@ -5,7 +5,34 @@ LEFT JOIN driver_details ON driver_details.driverId = driver_rankings.driverId
 WHERE position = 1 
 GROUP BY driver_rankings.driverId
 ORDER BY COUNT(driver_rankings.position) DESC
-LIMIT 5;
+LIMIT 1;
 
--- this is a test query
-SELECT * FROM race_status;
+-- Which F1 driver has the most podiums (1st, 2nd, 3rd)?
+SELECT driver_details.forename || ' ' || driver_details.surname as driverName, COUNT(driver_rankings.position) as first_places
+FROM driver_rankings 
+LEFT JOIN driver_details ON driver_details.driverId = driver_rankings.driverId 
+WHERE position <= 3
+GROUP BY driver_rankings.driverId
+ORDER BY COUNT(driver_rankings.position) DESC
+LIMIT 1;
+
+-- What's the fastest lap time of each driver?
+SELECT driver_details.forename || ' ' || driver_details.surname as driverName, lap_timings.time
+FROM lap_timings 
+LEFT JOIN driver_details ON driver_details.driverId = lap_timings.driverId 
+GROUP BY lap_timings.driverId
+ORDER BY MIN(lap_timings.milliseconds) ASC;
+
+-- For each driver, find the constructor they have driven for most.
+CREATE TEMPORARY TABLE most_common_team AS
+SELECT driverId, constructorId
+FROM (
+    SELECT 
+        driverId,
+        constructorId,
+        COUNT(*) AS team_count,
+        RANK() OVER (PARTITION BY driverId ORDER BY COUNT(*) DESC) AS rank
+    FROM race_results
+    GROUP BY driverId, constructorId
+) ranked_teams
+WHERE rank = 1;
